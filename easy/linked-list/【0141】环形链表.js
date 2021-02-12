84f235881b99be605c261272305895d0a9743999
qulongjun <154256698@qq.com>
2021-02-12 15:07:14 +0800

    【0141】环形链表

---
+++ easy/linked-list/【0141】环形链表.js
@@ -0,0 +1,102 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=javascript
+ *
+ * [141] 环形链表
+ *
+ * https://leetcode-cn.com/problems/linked-list-cycle/description/
+ *
+ * algorithms
+ * Easy (50.76%)
+ * Likes:    926
+ * Dislikes: 0
+ * Total Accepted:    330.1K
+ * Total Submissions: 650.4K
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * 给定一个链表，判断链表中是否有环。
+ *
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，我们使用整数 pos
+ * 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意：pos
+ * 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+ *
+ * 如果链表中存在环，则返回 true 。 否则，返回 false 。
+ *
+ *
+ *
+ * 进阶：
+ *
+ * 你能用 O(1)（即，常量）内存解决此问题吗？
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ *
+ * 输入：head = [1,2], pos = 0
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ *
+ *
+ * 示例 3：
+ *
+ *
+ *
+ * 输入：head = [1], pos = -1
+ * 输出：false
+ * 解释：链表中没有环。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 链表中节点的数目范围是 [0, 10^4]
+ * -10^5 <= Node.val <= 10^5
+ * pos 为 -1 或者链表中的一个 有效索引 。
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  // 边界条件，如果 head 为 null，则没有环
+  if (head === null) return false;
+  // 定义 first，last 两个指针，first 走一步， last 走两步
+  let first = head,
+    last = head.next;
+  // 当 first !== last 的时候，就继续往前走
+  while (first !== last) {
+    // 如果前面的指针已经为 null 或者即将为 null，则表示没有环
+    if (last === null || last.next === null) return false;
+    // first 节点往前走一步
+    first = first.next;
+    // last 节点万千走两步
+    last = last.next.next;
+  }
+  // 如果能结束循环，则表示找到环了
+  return true;
+};
+// @lc code=end
