19cea0cb1178eb265f96b65bf7f5eefe497637ed
qulongjun <154256698@qq.com>
2021-02-24 23:54:12 +0800

    【0104】二叉树的最大深度

--- easy/tree/【0104】二叉树的最大深度.js
+++ easy/tree/【0104】二叉树的最大深度.js
@@ -44,7 +44,7 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var maxDepth = function (root) {
+var maxDepth_v1 = function (root) {
   // 如果节点为 null，表示当前节点不存在
   if (root === null) return 0;
 
@@ -52,4 +52,33 @@ var maxDepth = function (root) {
   // maxDepth(root.left) + 1：当前节点为 1 层，加上左子树的层数/右子树的层数
   return Math.max(maxDepth(root.left) + 1, maxDepth(root.right) + 1);
 };
+
+// 层次遍历二叉树方式BFS
+var maxDepth = function (root) {
+  // 如果根节点为 null，直接返回 0
+  if (root === null) return 0;
+  // 定义一个变量存放深度，层次就是深度
+  let count = 0;
+  // 定义一个队列存放每一层的树的节点，一层就是 1 个深度，将子节点从队尾插入，从队头取出
+  let queue = [root];
+  // 当队列没有空，就证明当前层次遍历还没进行完成
+  while (queue.length !== 0) {
+    // 每层会走一次 while，其中 size 表示该层总共的节点数
+    count++;
+    // 暂存一下该层的节点数
+    let size = queue.length;
+    // 遍历该层的节点数（为什么要暂存节点数，因为队列会实时追加子节点，数量动态变化的）
+    for (let i = 0; i < size; i++) {
+      // 当前节点出队列
+      let tempNode = queue.shift();
+      // 将左子树入队列
+      if (tempNode.left !== null) queue.push(tempNode.left);
+      // 将右子树入队列
+      if (tempNode.right !== null) queue.push(tempNode.right);
+    }
+  }
+  // 返回深度
+  return count;
+};
+
 // @lc code=end
