a14ffda057d336975849d51b26ac1a7630030143
瞿龙俊 <154256698@qq.com>
2022-07-27 14:33:20 +0800

    【0160】相交链表

---
+++ 链表/【0160】相交链表/Solutions/solution_2.js
@@ -0,0 +1,158 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=javascript
+ *
+ * [160] 相交链表
+ *
+ * https://leetcode.cn/problems/intersection-of-two-linked-lists/description/
+ *
+ * algorithms
+ * Easy (63.00%)
+ * Likes:    1783
+ * Dislikes: 0
+ * Total Accepted:    519.8K
+ * Total Submissions: 824.5K
+ * Testcase Example:  '8\n[4,1,8,4,5]\n[5,6,1,8,4,5]\n2\n3'
+ *
+ * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+ *
+ * 图示两个链表在节点 c1 开始相交：
+ *
+ *
+ *
+ * 题目数据 保证 整个链式结构中不存在环。
+ *
+ * 注意，函数返回结果后，链表必须 保持其原始结构 。
+ *
+ * 自定义评测：
+ *
+ * 评测系统 的输入如下（你设计的程序 不适用 此输入）：
+ *
+ *
+ * intersectVal - 相交的起始节点的值。如果不存在相交节点，这一值为 0
+ * listA - 第一个链表
+ * listB - 第二个链表
+ * skipA - 在 listA 中（从头节点开始）跳到交叉节点的节点数
+ * skipB - 在 listB 中（从头节点开始）跳到交叉节点的节点数
+ *
+ *
+ * 评测系统将根据这些输入创建链式数据结构，并将两个头节点 headA 和 headB 传递给你的程序。如果程序能够正确返回相交节点，那么你的解决方案将被
+ * 视作正确答案 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ *
+ * 输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2,
+ * skipB = 3
+ * 输出：Intersected at '8'
+ * 解释：相交节点的值为 8 （注意，如果两个链表相交则不能为 0）。
+ * 从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,6,1,8,4,5]。
+ * 在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ *
+ *
+ * 输入：intersectVal = 2, listA = [1,9,1,2,4], listB = [3,2,4], skipA = 3, skipB
+ * = 1
+ * 输出：Intersected at '2'
+ * 解释：相交节点的值为 2 （注意，如果两个链表相交则不能为 0）。
+ * 从各自的表头开始算起，链表 A 为 [1,9,1,2,4]，链表 B 为 [3,2,4]。
+ * 在 A 中，相交节点前有 3 个节点；在 B 中，相交节点前有 1 个节点。
+ *
+ *
+ * 示例 3：
+ *
+ *
+ *
+ *
+ * 输入：intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+ * 输出：null
+ * 解释：从各自的表头开始算起，链表 A 为 [2,6,4]，链表 B 为 [1,5]。
+ * 由于这两个链表不相交，所以 intersectVal 必须为 0，而 skipA 和 skipB 可以是任意值。
+ * 这两个链表不相交，因此返回 null 。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * listA 中节点数目为 m
+ * listB 中节点数目为 n
+ * 1 <= m, n <= 3 * 10^4
+ * 1 <= Node.val <= 10^5
+ * 0 <= skipA <= m
+ * 0 <= skipB <= n
+ * 如果 listA 和 listB 没有交点，intersectVal 为 0
+ * 如果 listA 和 listB 有交点，intersectVal == listA[skipA] == listB[skipB]
+ *
+ *
+ *
+ *
+ * 进阶：你能否设计一个时间复杂度 O(m + n) 、仅用 O(1) 内存的解决方案？
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+  let first = headA,
+    second = headB,
+    isSkipFirst = false,
+    isSkipSecond = false;
+
+  /**
+   * 思路：双指针，第一个指针从 headA 开始，第二个指针从 headB 开始
+   * 依次往前走，当指针走到尾部时，则指向另一个链表的开头位置，继续往前走
+   * 当两个指针相遇的地方，就是交点
+   */
+
+  // 如果没找到两个指针相等，就一直往前走
+  while (first !== second) {
+    // 两个指针各自往前走一步
+    first = first.next;
+    second = second.next;
+
+    // 如果第一个指针为 null 了，判断当前是否已经交换到了第二个链表上
+    // 如果没交换过，则交换到第二个链表上
+    // 否则直接返回 null，查找结束
+    if (first === null) {
+      if (!isSkipFirst) {
+        isSkipFirst = true;
+        first = headB;
+      } else return null;
+    }
+
+    // 如果第二个指针为 null 了，判断当前是否已经交换到了第一个链表上
+    // 如果没交换过，则交换到第一个链表上
+    // 否则直接返回 null，查找结束
+    if (second === null) {
+      if (!isSkipSecond) {
+        isSkipSecond = true;
+        second = headA;
+      } else return null;
+    }
+  }
+
+  // 执行到这里的条件是 frist === second，即两个指针相等
+  return first;
+};
+// @lc code=end
